9fde88a2ba0fc9b27f7872abf173d5fa35f685f2
Kazuya-Sakashita <kz0508@gmail.com>
2021-10-27 07:36:44 +0900

    bundle exec rubocop -a実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,8 @@
 class ApplicationController < ActionController::Base
-
-    def after_sign_in_path_for(resource)
-        case resource
-        when User
-          root_path
-        end
-      end
-
-      
+  def after_sign_in_path_for(resource)
+    case resource
+    when User
+      root_path
+    end
+  end
 end
